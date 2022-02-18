ba26b89d895df02e3f6a7901ebc3eef60815a090
hsin-yu <tina2793778@gmail.com>
2022-02-18 11:28:37 +0800

    first commit

---
+++ wp-content/themes/nogoodstheme/index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<main class="pt-5">
+  <section>
+    <?php get_template_part('company'); ?>
+  </section>
+  <section class="my-5 py-5 bg-light">
+    <div class="container">
+      <?php get_template_part('about'); ?>
+    </div>
+  </section>
+  <section class="mb-5 container">
+    <?php get_template_part('assess'); ?>
+  </section>
+  <section class="bg-light">
+    <?php get_template_part('join'); ?>
+  </section>
+</main>
+<?php get_footer(); ?>
+  
